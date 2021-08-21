846f7ebb4d746d6b6c8e44775fd0061b632c3cb5
kottory <kottory@outlook.com>
2021-08-21 10:15:37 +0800

    first commit

---
+++ njupass/__init__.py
@@ -0,0 +1,131 @@
+"""
+DESCRIPTION:
+    Tools for getting Authorization websites of Nanjing University
+PACKAGES:
+    NjuUiaAuth
+    NjuEliteAuth
+"""
+import execjs
+import requests
+import re
+import os
+from io import BytesIO
+
+URL_NJU_UIA_AUTH = 'https://authserver.nju.edu.cn/authserver/login'
+URL_NJU_ELITE_LOGIN = 'http://elite.nju.edu.cn/jiaowu/login.do'
+
+
+class NjuUiaAuth:
+    """
+    DESCRIPTION:
+        Designed for passing Unified Identity Authentication(UIA) of Nanjing University.
+    """
+
+    def __init__(self):
+        self.session = requests.Session()
+        self.session.headers.update({
+           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0'
+        })
+
+        r = self.session.get(URL_NJU_UIA_AUTH)
+        self.lt = re.search(
+            r'<input type="hidden" name="lt" value="(.*)"/>', r.text).group(1)
+        self.execution = re.search(
+            r'<input type="hidden" name="execution" value="(.*)"/>', r.text).group(1)
+        self._eventId = re.search(
+            r'<input type="hidden" name="_eventId" value="(.*)"/>', r.text).group(1)
+        self.rmShown = re.search(
+            r'<input type="hidden" name="rmShown" value="(.*)"', r.text).group(1)
+        self.pwdDefaultEncryptSalt = re.search(
+            r'<input type="hidden" id="pwdDefaultEncryptSalt" value="(.*)"', r.text).group(1)
+
+    def getCaptchaCode(self):
+        """
+        DESCRIPTION:
+            Getting captcha code binded with IP
+        RETURN_VALUE:
+            captcha code image(ByteIO). Recommended using Image.show() in PIL.
+        """
+        url = 'https://authserver.nju.edu.cn/authserver/captcha.html'
+        res = self.session.get(url, stream=True)
+        return BytesIO(res.content)
+
+    def parsePassword(self, password):
+        """
+        DESCRIPTION:
+            Parsing password to encrypted form which can be identified by the backend sersver of UIA.
+        ATTRIBUTES:
+            password(str): Original password
+        """
+        with open(os.path.join(os.path.dirname(__file__), 'resources/encrypt.js')) as f:
+            ctx = execjs.compile(f.read())
+        return ctx.call('encryptAES', password, self.pwdDefaultEncryptSalt)
+
+    def needCaptcha(self, username):
+        url = 'https://authserver.nju.edu.cn/authserver/needCaptcha.html?username={}'.format(
+            username)
+        r = self.session.post(url)
+        if 'true' in r.text:
+            return True
+        else:
+            return False
+
+    def login(self, username, password, captchaResponse=""):
+        """
+        DESCRIPTION:
+            Post a request for logging in.
+            Return true if login success, false otherwise
+        ATTRIBUTES:
+            username(str)
+            password(str)
+        """
+        data = {
+            'username': username,
+            'password': self.parsePassword(password),
+            'lt': self.lt,
+            'dllt': 'userNamePasswordLogin',
+            'execution': self.execution,
+            '_eventId': self._eventId,
+            'rmShown': self.rmShown,
+            'captchaResponse': captchaResponse,
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit 537.36 (KHTML, like Gecko) Chrome"
+        }
+        r = self.session.post(URL_NJU_UIA_AUTH, data=data,
+                              allow_redirects=False)
+        return r.status_code == 302
+
+
+class NjuEliteAuth:
+    """
+    DESCRIPTION:
+        Designed for passing Unified Identity Authentication(UIA) of Nanjing University.
+    """
+
+    def __init__(self):
+        self.session = requests.session()
+
+    def getValidateCode(self):
+        """
+        DESCRIPTION:
+            Getting validate code binded with IP
+        RETURN_VALUE:
+            validate code image(ByteIO). Recommended using Image.show() in PIL.
+        """
+        url = 'http://elite.nju.edu.cn/jiaowu/ValidateCode.jsp'
+        res = self.session.get(url, stream=True)
+        return BytesIO(res.content)
+
+    def login(self, userName, password, validateCode):
+        """
+        DESCRIPTION:
+            Post a request for logging in.
+        ATTRIBUTES:
+            username(str)
+            password(str)
+            validateCode(str)
+        """
+        self.session.post(URL_NJU_ELITE_LOGIN, data={
+            'userName': userName,
+            'password': password,
+            'ValidateCode': validateCode
+        })
---
+++ run.py
@@ -0,0 +1,46 @@
+from njupass import NjuUiaAuth
+from dotenv import load_dotenv
+import os
+import json
+import time
+
+URL_JKDK_LIST = 'http://ehallapp.nju.edu.cn/xgfw/sys/yqfxmrjkdkappnju/apply/getApplyInfoList.do'
+URL_JKDK_APPLY = 'http://ehallapp.nju.edu.cn/xgfw/sys/yqfxmrjkdkappnju/apply/saveApplyInfos.do'
+
+auth = NjuUiaAuth()
+
+if __name__ == "__main__":
+    load_dotenv(verbose=True)
+    username = os.getenv('NJU_USERNAME')
+    password = os.getenv('NJU_PASSWORD')
+    curr_location = os.getenv('CURR_LOCATION')
+
+    if auth.needCaptcha(username):
+        print("Captcha is not yet supported, please log in manually.")
+        os._exit(1)
+
+    ok = auth.login(username, password)
+    if not ok:
+        print("Login Failed.")
+        os._exit(1)
+
+    print('Login Success')
+
+    for count in range(10):
+        r = auth.session.get(URL_JKDK_LIST)
+        if r.status_code != 200:
+            break
+
+        dk_info = json.loads(r.text)['data'][0]
+        if dk_info['TBZT'] == "0":
+            wid = dk_info['WID']
+            data = "?WID={}&IS_TWZC=1&CURR_LOCATION={}&JRSKMYS=1&IS_HAS_JKQK=1&JZRJRSKMYS=1".format(wid, curr_location)
+            url = URL_JKDK_APPLY + data
+            auth.session.get(url)
+            time.sleep(1)
+        else:
+            print("Apply Success!")
+            os._exit(0)
+
+    print("Apply Failed!")
+    os._exit(0)
