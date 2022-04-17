d02c2bcc56701792ca1f3be01cc2ada8e8d2941b
psn417 <psn417@icloud.com>
2022-04-17 16:10:28 +0800

    fix

--- njupass/__init__.py
+++ njupass/__init__.py
@@ -24,7 +24,7 @@ class NjuUiaAuth:
     def __init__(self):
         self.session = requests.Session()
         self.session.headers.update({
-           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0'
+           'User-Agent': "Mozilla/5.0 (Linux; Android 11; M2006J10C Build/RP1A.200720.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/87.0.4280.141 Mobile Safari/537.36  cpdaily/8.2.7 wisedu/8.2.7})"
         })
 
         r = self.session.get(URL_NJU_UIA_AUTH)
