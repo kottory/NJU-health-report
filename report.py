abe74b81cd606071873f4c0a3239cc260a3d7b84
kottory <kottory@outlook.com>
2022-05-11 09:06:09 +0000

    fix

--- report.py
+++ report.py
@@ -1,5 +1,4 @@
 from njupass import NjuUiaAuth
-import json
 import time
 import datetime
 from pytz import timezone
@@ -7,7 +6,7 @@ from urllib.parse import urlencode
 
 URL_JKDK_LIST = 'http://ehallapp.nju.edu.cn/xgfw/sys/yqfxmrjkdkappnju/apply/getApplyInfoList.do'
 URL_JKDK_APPLY = 'http://ehallapp.nju.edu.cn/xgfw/sys/yqfxmrjkdkappnju/apply/saveApplyInfos.do'
-URL_JDKD_INDEX = 'http://ehallapp.nju.edu.cn/xgfw/sys/mrjkdkappnju/index.html'
+URL_JKDK_INDEX = 'http://ehallapp.nju.edu.cn/xgfw/sys/mrjkdkappnju/index.do'
 
 
 def get_zjhs_time(method='YESTERDAY'):
@@ -24,15 +23,25 @@ def apply(curr_location, logger, auth: NjuUiaAuth, covidTestMethod='YESTERDAY', 
     :param `covidTestMethod`: 最近核酸时间的方案
     :param `force`: 是否在今日已经打卡的前提下强制打卡
     """
+
+    headers = {
+        # required since 2022/4/20
+        'referer': 'http://ehallapp.nju.edu.cn/xgfw/sys/mrjkdkappnju/index.html',
+        "X-Requested-With": "com.wisedu.cpdaily.nju",
+        "User-Agent": "Mozilla/5.0 (Linux; Android 11; M2006J10C Build/RP1A.200720.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/92.0.4515.131 Mobile Safari/537.36 cpdaily/8.2.7 wisedu/8.2.7",
+        "Host": "ehallapp.nju.edu.cn",
+    }
     for _ in range(10):
         logger.info('尝试获取打卡列表信息...')
-        r = auth.session.get(URL_JKDK_LIST)
+
+        auth.session.get(URL_JKDK_INDEX)
+        r = auth.session.get(URL_JKDK_LIST, headers=headers)
         if r.status_code != 200:
             logger.error('获取失败，一分钟后再次尝试...')
             time.sleep(60)
             continue
 
-        dk_info = json.loads(r.text)['data'][0]
+        dk_info = r.json()['data'][0]
 
         has_applied = dk_info['TBZT'] == "1"
         wid = dk_info['WID']
@@ -44,13 +53,9 @@ def apply(curr_location, logger, auth: NjuUiaAuth, covidTestMethod='YESTERDAY', 
             'JRSKMYS': 1,  # 今日苏康码颜色
             'IS_HAS_JKQK': 1,  # 健康情况
             'JZRJRSKMYS': 1,  # 居住人今日苏康码颜色
-            'SFZJLN': 0
+            'SFZJLN': 0,  # 是否最近离宁
         }
-        headers = {
-            'referer': URL_JDKD_INDEX,  # required since 2022/4/20
-            "X-Requested-With": "com.wisedu.cpdaily.nju",
-            "User-Agent": "Mozilla/5.0 (Linux; Android 11; M2006J10C Build/RP1A.200720.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/92.0.4515.131 Mobile Safari/537.36 cpdaily/8.2.7 wisedu/8.2.7"
-        }
+
         url = URL_JKDK_APPLY + '?' + urlencode(param)
 
         if not has_applied or force:
